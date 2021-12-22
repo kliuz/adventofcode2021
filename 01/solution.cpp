a537218c4e0f63959deda1f25880b0b0e57f7093
kliuz <33242952+kliuz@users.noreply.github.com>
2021-12-21 23:00:40 -0500

    Add solution for day 1 part 2

--- 01/solution.cpp
+++ 01/solution.cpp
@@ -1,14 +1,12 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    string filename = "input.txt";
-    ifstream ifs {filename};
-
+int part1(ifstream&& ifs) {
     if (!ifs) {
-        cerr << "Couldn't open input file " << filename << endl;
+        cerr << "Couldn't open input file for part 1" << endl;
     }
 
     string line;
@@ -25,5 +23,39 @@ int main() {
         }
         curr = val;
     }
-    cout << "Number of increases: " << num_increases << endl;
+    return num_increases;
+}
+
+int part2(ifstream&& ifs, int window_size) {
+    if (!ifs) {
+        cerr << "Couldn't open input file for part 1" << endl;
+    }
+
+    string line;
+    vector<int> window;
+    int num_increases = 0;
+    while (getline(ifs, line)) {
+        int val = stoi(line);
+        if (window.size() < window_size) {
+            window.push_back(val);
+            continue;
+        }
+        vector<int>::iterator it = window.begin();
+        if (val > *it) {
+            num_increases++;
+        }
+
+        window.push_back(val);
+        window.erase(window.begin());
+    }
+    return num_increases;
+}
+
+
+int main() {
+    string filename = "input.txt";
+    ifstream&& ifs {filename};
+
+    cout << "Number of increases: " << part1(ifstream(filename)) << endl;
+    cout << "Number of increases: " << part2(ifstream(filename), 3) << endl;
 }
