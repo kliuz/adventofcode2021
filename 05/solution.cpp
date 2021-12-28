2950d313d06a569d1ad507f50053cd41b0cf8505
kliuz <33242952+kliuz@users.noreply.github.com>
2021-12-27 19:31:24 -0500

    Add solution for day 5 part 1

---
+++ 05/solution.cpp
@@ -0,0 +1,77 @@
+#include <boost/functional/hash.hpp>
+#include <fstream>
+#include <iostream>
+#include <string.h>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+pair<int, int> get_coord(char *coord) {
+  char *token = strtok(coord, ",");
+  int first = stoi(token, nullptr, 10);
+  token = strtok(nullptr, ",");
+  int second = stoi(token, nullptr, 10);
+
+  return make_pair(first, second);
+}
+
+void insert_or_update(
+    unordered_map<pair<int, int>, int, boost::hash<pair<int, int>>>
+        &coord_to_lines,
+    pair<int, int> coord) {
+  if (coord_to_lines.count(coord) == 0) {
+    coord_to_lines.insert({coord, 1});
+  } else {
+    int& num_intersections = coord_to_lines.at(coord);
+    num_intersections += 1;
+  }
+}
+
+int part1(ifstream &&ifs) {
+  if (!ifs) {
+    cerr << "Couldn't open input file" << endl;
+  }
+
+  unordered_map<pair<int, int>, int, boost::hash<pair<int, int>>>
+      coord_to_lines;
+  string line;
+  while (getline(ifs, line)) {
+    char *first_pair = strtok(&line[0], " ");
+    strtok(nullptr, " "); // Skip "->" token
+    char *second_pair = strtok(nullptr, " ");
+
+    pair<int, int> start = get_coord(first_pair);
+    pair<int, int> end = get_coord(second_pair);
+
+    if (start.first == end.first) {
+      int lo = start.second < end.second ? start.second : end.second;
+      int hi = start.second > end.second ? start.second : end.second;
+      for (int i = lo; i <= hi; i++) {
+        insert_or_update(coord_to_lines, make_pair(start.first, i));
+      }
+    } else if (start.second == end.second) {
+      int lo = start.first < end.first ? start.first : end.first;
+      int hi = start.first > end.first ? start.first : end.first;
+      for (int i = lo; i <= hi; i++) {
+        insert_or_update(coord_to_lines, make_pair(i, start.second));
+      }
+    }
+  }
+
+  int count = 0;
+  for (auto& entry : coord_to_lines) {
+    if (entry.second >= 2) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+int main() {
+  string filename = "input.txt";
+
+  int part1_answer = part1(ifstream(filename));
+  cout << "Answer: " << part1_answer << endl;
+}
